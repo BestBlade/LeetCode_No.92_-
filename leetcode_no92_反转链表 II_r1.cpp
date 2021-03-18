77c1b9a1c6c57ca5a667b87717175bbf8c658d1d
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-03-18 10:12:37 +0800

    Add files via upload

---
+++ leetcode_no92_反转链表 II_r1.cpp
@@ -0,0 +1,83 @@
+﻿/* ------------------------------------------------------------------|
+反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
+
+说明:
+1 ≤ m ≤ n ≤ 链表长度。
+
+示例:
+
+输入: 1->2->3->4->5->NULL, m = 2, n = 4
+输出: 1->4->3->2->5->NULL
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/reverse-linked-list-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	
+*
+*	执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+*	内存消耗：7.2 MB, 在所有 C++ 提交中击败了57.69%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+ListNode* reverseBetween(ListNode* head, int left, int right) {
+    ListNode dummy(0);
+    dummy.next = head;
+    ListNode* pre = &dummy;
+    for (int i = 1; i < left; ++i) {
+        pre = pre->next;
+    }
+
+    ListNode* curr = pre->next;
+    //pre->curr->next1->next2->next3....
+    //每循环一次，就把curr的下一个元素提到pre后面
+    //pre->next1->curr->next2->next3...
+    //pre->next2->next1->curr->next3...
+    for (int i = left; i < right; ++i) {
+        ListNode* next = curr->next;
+        curr->next = next->next;
+        next->next = pre->next;
+        pre->next = next;
+    }
+    return dummy.next;
+}
